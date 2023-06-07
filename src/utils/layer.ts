b048e05099e82d2cdafef93b7be95874af892694
Hiroshi Sugimori <hiroshi.sugimori@mapbox.com>
2023-06-07 17:24:50 +0900

    ADS-1609 update icon size

--- src/utils/layer.ts
+++ src/utils/layer.ts
@@ -8,20 +8,20 @@ export const createLayer = (id: string, sourceId: string): mapboxgl.SymbolLayer 
     'icon-image': ['get', 'icon'],
     'icon-size': [
       'interpolate',
-      ['exponential', 1.5],
+      ['linear', 1.5],
       ['zoom'],
-      10, 0.5, // zoom is 10 (or less)    -> icon size will be 0.5
-      16, 1.0, // zoom is 16 (or greater) -> icon size will be 1.0 
+      17, 0.5, // zoom is 17 (or less)    -> icon size will be 0.5
+      20, 1.0, // zoom is 20 (or greater) -> icon size will be 1.0
     ],
     'icon-anchor': 'bottom',
     'text-field': ['get', 'name'],
     'text-anchor': 'top',
     'text-size': [
       'interpolate',
-      ['exponential', 1.5],
+      ['linear', 1.5],
       ['zoom'],
-      10, 9,  // zoom is 10 (or less)    -> text size will be 9
-      16, 12, // zoom is 16 (or greater) -> text size will be 12
+      17, 9,  // zoom is 17 (or less)    -> text size will be 9
+      20, 12, // zoom is 20 (or greater) -> text size will be 12
     ]
   },
   paint: {
